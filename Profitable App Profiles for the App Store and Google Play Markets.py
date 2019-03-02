b302e3c4dd29d272b1b2aad17d249cd6d59c5e8d
Raju Chepuri <rajuchepuri7@gmail.com>
2019-03-02 20:30:22 +0530

    This is .py file

--- Profitable App Profiles for the App Store and Google Play Markets.py
+++ Profitable App Profiles for the App Store and Google Play Markets.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Profitable App Profiles for the App Store and Google Play Markets
+## Profitable App Profiles for the App Store and Google Play Markets
 # 
-# Our aim in this project is to find mobile app profiles that are profitable for the App Store and Google Play markets. We're working as data analysts for a company that builds Android and iOS mobile apps, and our job is to enable our team of developers to make data-driven decisions with respect to the kind of apps they build.
+# Our aim in this project is to find mobile app profiles that are profitable for the App Store and Google Play markets. Assume that We're working as data analysts for a company that builds Android and iOS mobile apps, and our job is to enable our team of developers to make data-driven decisions with respect to the kind of apps they build.
 # 
-# At our company, we only build apps that are free to download and install, and our main source of revenue consists of in-app ads. This means that our revenue for any given app is mostly influenced by the number of users that use our app. Our goal for this project is to analyze data to help our developers understand what kinds of apps are likely to attract more users.
+# At this company, we only build apps that are free to download and install, and our main source of revenue consists of in-app ads. This means that our revenue for any given app is mostly influenced by the number of users that use our app. Our goal for this project is to analyze data to help our developers understand what kinds of apps are likely to attract more users.
 
 # ## Opening and Exploring the Data
 # As of September 2018, there were approximately 2 million iOS apps available on the App Store, and 2.1 million Android apps on Google Play.
@@ -560,9 +560,3 @@ for app in android_apps_final:
 # In this project, we analyzed data about the App Store and Google Play mobile apps with the goal of recommending an app profile that can be profitable for both markets.
 # 
 # We concluded that taking a popular book (perhaps a more recent book) and turning it into an app could be profitable for both the Google Play and the App Store markets. The markets are already full of libraries, so we need to add some special features besides the raw version of the book. This might include daily quotes from the book, an audio version of the book, quizzes on the book, a forum where people can discuss the book, etc
-
-# In[ ]:
-
-
-
-
